ef75a79c276a25f8a72e0662b81c8521f8b6f3ae
Albert Gumbs <albertgumbs38@gmail.com>
2024-10-22 20:57:52 -0400

    Updated React

--- src/components/Predictions.js
+++ src/components/Predictions.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import './css/App.css';
-import Game from './Game'
-import data from './today.json'
+import Game from './Game';
+import data from '../today.json';
 
 function Predictions() {
   const [games, setGames] = useState([]);
 
+  useEffect(() => {
+    const arr = Object.values(data);
+    console.log(arr);
+    setGames(arr);
+  }, []); // Empty dependency array ensures this runs only once
+  
   if (Object.keys(data).length === 0) {
-    return(
+    console.log('No games today');
+    return (
       <div>
         <h1>No games today</h1>
-      </ div>
+      </div>
     );
-    console.log('No games today');
-  } else {
-    setGames(data);
   }
-
 
   return (
     <div className="App">
@@ -26,10 +29,10 @@ function Predictions() {
           key={index}
           homeTeam={game.home_team_name}
           homeLogo={game.home_team_logo}
-          homePercentage={Math.round(game.prediction*100, 2)}
+          homePercentage={Math.round(game.prediction * 100, 2)}
           awayTeam={game.away_team_name}
           awayLogo={game.away_team_logo}
-          awayPercentage={Math.round((1 - game.prediction)*100, 2)}
+          awayPercentage={Math.round((1 - game.prediction) * 100, 2)}
         />
       ))}
     </div>
