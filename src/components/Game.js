449e843770324f4f4d1ed9509b4361b3b21fb574
Albert Gumbs <albertgumbs38@gmail.com>
2024-10-23 00:56:16 -0400

    Added logos and edited site styles

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,25 +4,27 @@ import './css/Game.css';
 
 function Game({homeTeam, homeLogo, homePercentage, awayTeam, awayLogo, awayPercentage}) {
   return (
-    <div className="game">
-      <div className="team-container">
-        <img className="team-logo" src={homeLogo} />
-        <div className="team-info">
-          <div className="team">{homeTeam}</div>
-          <p className="percentage">{homePercentage}%</p>
+    <div className="Game">
+      <div className="game-container">
+        <div className="team-container">
+          <img className="team-logo" src={awayLogo} style={{float: 'left'}}/>
+          <div className="team-info">
+            <div className="team">{awayTeam}</div>
+            <p className="percentage">{awayPercentage}%</p>
+          </div>
         </div>
-      </div>
-      
-      <p className="vs">@</p>
-      
-      <div className="team-container">
-        <div className="team-info">
-          <div className="team">{awayTeam}</div>
-          <p className="percentage">{awayPercentage}%</p>
+        
+        <p className="vs">vs</p>
+        
+        <div className="team-container">
+          <div className="team-info">
+            <div className="team">{homeTeam}</div>
+            <p className="percentage">{homePercentage}%</p>
+          </div>
+          <img className="team-logo" src={homeLogo} style={{float: 'right'}}/>
         </div>
-        <img className="team-logo" src={awayLogo} />
+        <div style={{ borderBottom: '1px solid black' }}></div>
       </div>
-      <div style={{ borderBottom: '1px solid black' }}></div>
     </div>
   );
 }
--- src/components/Predictions.js
+++ src/components/Predictions.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './css/App.css';
+import './css/Predictions.css';
 import Game from './Game';
 import data from '../today.json';
 
@@ -10,29 +11,29 @@ function Predictions() {
     const arr = Object.values(data);
     console.log(arr);
     setGames(arr);
-  }, []); // Empty dependency array ensures this runs only once
-  
+  }, []);
+
   if (Object.keys(data).length === 0) {
     console.log('No games today');
     return (
       <div>
-        <h1>No games today</h1>
+        <h1 className='pred-header'>No games today</h1>
       </div>
     );
   }
 
   return (
     <div className="App">
-      <h1>Games</h1>
+      <h1 className='pred-header'>Games</h1>
       {games.map((game, index) => (
         <Game
           key={index}
           homeTeam={game.home_team_name}
           homeLogo={game.home_team_logo}
-          homePercentage={Math.round(game.prediction * 100, 2)}
+          homePercentage={Math.round((1 - game.prediction) * 100, 2)}
           awayTeam={game.away_team_name}
           awayLogo={game.away_team_logo}
-          awayPercentage={Math.round((1 - game.prediction) * 100, 2)}
+          awayPercentage={Math.round(game.prediction * 100, 2)}
         />
       ))}
     </div>
